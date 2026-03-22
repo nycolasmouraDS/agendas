72526eb8636bfc99927f4bffc272af0b90c7dac9
Nycolas <268440060+nycolasmouraDS@users.noreply.github.com>
2026-03-21 23:06:36 -0300

    Ajustes

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ tempo_medio = int(input('Uso diário em horas:'))
 consumo_mensal = potencia_do_aparelho * tempo_medio * 30 / 1000
 
 #Saída
-print(f'O ítem {nome_do_aparelho} tem {potencia_do_aparelho}watts de potência')
-print(f'De acordo com seu uso diário de {tempo_medio}h o custo mensal é {consumo_mensal}reais por esse elêtronico')
+print(f'{nome_do_aparelho} tem {potencia_do_aparelho}watts de potência')
+print(f'De acordo com seu uso diário de {tempo_medio}h o consumo mensal é {consumo_mensal}kWh por esse elêtronico')
